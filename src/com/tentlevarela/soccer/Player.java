bb1d454537899732635056a3627d1cb77d3ed4ad
Ricardo Hernandez <ricardo.hernandez@base22.com>
2018-04-23 20:20:05 -0500

    Add Player and Team interfaces

---
+++ src/com/tentlevarela/soccer/Player.java
@@ -0,0 +1,7 @@
+package com.tentlevarela.soccer;
+
+public interface Player {
+
+	public Team getTeam(); 
+	
+}
---
+++ src/com/tentlevarela/soccer/Team.java
@@ -0,0 +1,7 @@
+package com.tentlevarela.soccer;
+
+public interface Team {
+	
+	public String getInfo();
+
+}
